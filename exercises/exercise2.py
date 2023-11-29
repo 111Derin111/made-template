5108836275a2033f36cd20a175627547a7398f15
111Derin111 <152343938+111Derin111@users.noreply.github.com>
2023-11-29 01:58:06 +0100

    Update exercise2.py

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,30 +1,27 @@
 import pandas as pd
 import sqlite3
 
-data_url="https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
-df= pd.read_csv(data_url,sep=';')
+# Step 1: Read data from the CSV file
+data_url = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
+df = pd.read_csv(data_url, sep=';')
 
-df.drop("Status",axis=1,inplace= True)
+# Step 2: Drop the "Status" column
+df.drop("Status", axis=1, inplace=True)
 
-Verkehr_list=("FV", "RV", "nur DPN")
+# Step 3: Filter rows with valid values
+Verkehr_list = ["FV", "RV", "nur DPN"]
 df = df[df['Verkehr'].isin(Verkehr_list)]
 
-df.Laenge.replace(to_replace=",", value=".", inplace=True,regex=True)
-df.Breite.replace(to_replace=",", value=".", inplace=True,regex=True)
+df['Laenge'] = pd.to_numeric(df['Laenge'].str.replace(',', '.'), errors='coerce')
+df['Breite'] = pd.to_numeric(df['Breite'].str.replace(',', '.'), errors='coerce')
 
+df = df[(df['Laenge'] >= -90) & (df['Laenge'] <= 90)]
+df = df[(df['Breite'] >= -90) & (df['Breite'] <= 90)]
 
-df = df.astype({'Laenge':'float'})
-df = df.astype({'Breite':'float'})
+pattern = r"^[A-Za-z]{2}:\d*:\d*(?::\d*)?$"
+df = df[df['IFOPT'].str.match(pattern, na=False)]
 
-df = df[(df.Laenge <= 90.0) & (df.Laenge >= -90.0)]
-
-
-
-pattern = "^[A-Za-z]{2}:\d*:\d*(?::\d*)?$"
-df.IFOPT.str.match(pattern)
-
-df=df[df.IFOPT.str.match(pattern)]
-
+# Step 4: Create SQLite database and insert data
 conn = sqlite3.connect('trainstops.sqlite')
 cursor = conn.cursor()
 
@@ -41,10 +38,13 @@ sqlite_types = {
     'IFOPT': 'TEXT',
 }
 
+# Create the "trainstops" table
 create_table_query = f"CREATE TABLE trainstops ({', '.join([f'{col} {sqlite_types[col]}' for col in df.columns])})"
 cursor.execute(create_table_query)
 
+# Insert data into the "trainstops" table
 df.to_sql('trainstops', conn, if_exists='replace', index=False)
 
+# Commit changes and close the connection
 conn.commit()
 conn.close()
